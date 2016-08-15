50da84a6066d0cc0085d740ffbf32fb92d24b047
Souphan <souphan.ctr@gmail.com>
2016-08-15 13:22:12 -0500

    added js file and style

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDKHaSWKGT-TDOqPFOUJoLupuhzW2hxrho",
+        authDomain: "messagetemplates-f0228.firebaseapp.com",
+        databaseURL: "https://messagetemplates-f0228.firebaseio.com",
+        storageBucket: "messagetemplates-f0228.appspot.com",
+    };
+    firebase.initializeApp(config);
+
+});
